0f683a3895d3f47070459cba6fcfaf4710934cc2
TunrayoAkin <tunrayo.akinkunle@hotmail.co.uk>
2017-12-29 18:44:57 +0000

    added google maps

---
+++ js/map.js
@@ -0,0 +1,12 @@
+   function initMap() {
+        var uluru = {lat: 52.630509, lng: -1.139349};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+
